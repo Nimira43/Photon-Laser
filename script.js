1e7a5e2dfcbadd7a6fb2461f6dd33933579bfb02
Nick Rabone <n.rabone72@aol.com>
2024-07-21 22:37:50 +0100

    Created basic player and projectiles.

---
+++ script.js
@@ -0,0 +1,71 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  constructor(x, y, radius, colour) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.colour = colour
+  }
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.colour
+    c.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, colour, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.colour = colour
+    this.velocity = velocity
+  }
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.colour
+    c.fill()
+  }
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+const player = new Player(x, y, 30, '#ff4500')
+const projectiles = []
+
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw()
+  projectiles.forEach((projectile) => {
+    projectile.update()
+  })
+}
+
+addEventListener('click', (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  )
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, '#ffd700', velocity)
+  )
+}) 
+
+animate()
